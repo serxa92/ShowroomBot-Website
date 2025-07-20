1280703676bbcf13047e8f333634c4f09ccfbf70
Sergio <serxa92@hotmail.com>
2025-07-20 11:46:54 +0200

    feat: actualizar metadatos y configuración de Open Graph para el sitio web de ShowroomBot

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,43 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'ShowroomBot',
-  description: 'ShowroomBot es un bot de Discord para publicar y compartir proyectos dentro de un canal showroom. Ideal para comunidades de programadores o estudiantes que quieren mostrar lo que están construyendo . ',
-  
+  title: 'ShowroomBot - Website',
+  description:
+    'ShowroomBot es un bot de Discord para publicar y compartir proyectos dentro de un canal showroom. Ideal para comunidades de programadores o estudiantes que quieren mostrar lo que están construyendo.',
+  keywords: ['ShowroomBot', 'Discord bot', 'proyectos', 'programadores', 'comunidad', 'estudiantes', 'dev'],
+  authors: [{ name: 'Serxa92' }],
+  creator: 'Serxa92',
+  metadataBase: new URL('https://showroom-bot-website.vercel.app/'),
+  openGraph: {
+    title: 'ShowroomBot - Website',
+    description:
+      'ShowroomBot es un bot de Discord para publicar y compartir proyectos dentro de un canal showroom. Ideal para comunidades de programadores o estudiantes que quieren mostrar lo que están construyendo.',
+    url: 'https://showroom-bot-website.vercel.app/',
+    siteName: 'ShowroomBot - Website',
+    images: [
+      {
+        url: '/public/captura.png', 
+        width: 1200,
+        height: 630,
+        alt: 'Vista previa de la Web',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'ShowroomBot',
+    description:
+      'ShowroomBot es un bot de Discord para publicar y compartir proyectos dentro de un canal showroom. Ideal para comunidades de programadores o estudiantes que quieren mostrar lo que están construyendo.',
+    creator: '@Serxa92',
+    images: ['/public/captura.png'],
+  },
+  icons: {
+    icon: '/favicon.png',
+    shortcut: '/favicon.png',
+    apple: '/favicon.png',
+  },
+  themeColor: '#0a0a0a',
 }
 
 export default function RootLayout({
